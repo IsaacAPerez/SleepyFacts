bdbdd019afad185d7682811a89b7b3a243051219
IsaacAPerez <iperez2435@gmail.com>
2020-11-21 09:33:04 -0800

    First push to master

---
+++ Sleepy/SleepyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepyApp.swift
+//  Sleepy
+//
+//  Created by Isaac Perez on 10/31/20.
+//
+
+import SwiftUI
+
+@main
+struct SleepyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
