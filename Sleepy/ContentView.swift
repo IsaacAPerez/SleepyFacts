08addf4b8036437068fa2e520c7801059a075ba1
IsaacAPerez <iperez2435@gmail.com>
2020-11-24 17:41:39 -0800

    Added Button

--- Sleepy/ContentView.swift
+++ Sleepy/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Text("Hello, world!")
+                .fontWeight(.bold)
+                .font(.largeTitle)
+                .padding()
+                
+
+            
+            Spacer()
+            Button(action: {
+                print("Delete button tapped!")
+            }) {
+                Text("Hello World")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .frame(minWidth: 0, maxWidth: .some(350))
+                    .padding()
+                    .background(Color.red)
+                    .cornerRadius(40)
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 35)
+                            .stroke(Color.red, lineWidth: 5)
+                    )
+            }
+            
+        }
     }
 }
 
